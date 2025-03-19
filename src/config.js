6e10272f3df7002bd38fae4303100a7217d444d2
maxgul222 <maxgul@example.com>
2025-03-19 16:49:38 +1000

    Update webapp URL to Netlify deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     BOT_TOKEN: '7634341504:AAERgojNZd5dN98iXZCf-9pHKQZ-Oz5T1cA', // Токен от BotFather
-    WEBAPP_URL: 'https://yandex.ru/', // URL вашего веб-приложения
+    WEBAPP_URL: 'https://fddddfds.netlify.app/games', // URL вашего веб-приложения
     INITIAL_BALANCE: 1000, // Начальный баланс для новых пользователей
 };
 
